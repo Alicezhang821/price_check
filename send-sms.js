3b757c10c7052eb6e7988be85302280e789b4513
Alice <48072065+Alicezhang821@users.noreply.github.com>
2026-02-02 21:00:07 +1100

    Update send-sms.js

--- send-sms.js
+++ send-sms.js
@@ -1,21 +1,22 @@
-import Twilio from 'twilio';
+// send-sms.js
+const Twilio = require('twilio');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const fromNumber = process.env.TWILIO_FROM_NUMBER;
-const toNumber = process.env.TO_NUMBER;  // recipient
+const toNumber = process.env.TO_NUMBER;
 const messageBody = process.env.MESSAGE_BODY;
 
 const client = Twilio(accountSid, authToken);
 
-try {
-  const message = await client.messages.create({
+client.messages
+  .create({
     body: messageBody,
     from: fromNumber,
     to: toNumber,
+  })
+  .then(message => console.log('Message sent! SID:', message.sid))
+  .catch(err => {
+    console.error('Failed to send SMS:', err);
+    process.exit(1);
   });
-  console.log('Message sent! SID:', message.sid);
-} catch (err) {
-  console.error('Failed to send SMS:', err);
-  process.exit(1);
-}
