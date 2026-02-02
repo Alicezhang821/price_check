55da48a27b6c248bf04b97cb53163b5529a22a40
aijing.zhang <aijing.zhang@ibm.com>
2026-02-02 19:03:51 +1100

    save

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-
+import fs from 'fs';
 test('Price alert', async ({ page }) => {
   await page.goto('https://www.perthmint.com/shop/bullion/cast-bars/perth-mint-1oz-gold-cast-bar/');
   await page.locator(`button#onetrust-reject-all-handler`).click();
@@ -19,6 +19,7 @@ test('Price alert', async ({ page }) => {
     // Convert "6,709.79" → 6709.79
     const price = Number(priceText.replace(/,/g, '').trim());
     console.log(price);
+    fs.writeFileSync('price.txt', price.toString());
     expect(price).toBeGreaterThan(6000);
   } else {
     console.log('Out of stock');
